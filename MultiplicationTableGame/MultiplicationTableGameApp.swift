3906a99a3d04a2ab4f756ebd317c37315f1b7144
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-03-12 16:22:54 -0700

    Initial Commit

---
+++ MultiplicationTableGame/MultiplicationTableGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableGameApp.swift
+//  MultiplicationTableGame
+//
+//  Created by Kenneth Oliver Rathbun on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
