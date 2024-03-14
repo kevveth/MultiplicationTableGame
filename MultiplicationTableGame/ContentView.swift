da4067a013bd9ccd8e8111dcc702da4f381cfcd2
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-03-13 21:07:42 -0700

    Animated the symbol representing the times table the player will play with. I was struggling to get the easeIn animation to trigger but found a solution through amosgyamfi's github repo containing swiftui animations. I further updated it to use more modern concurrency syntax in favor of using DispatchQueue.main and am happy with the results.

--- MultiplicationTableGame/ContentView.swift
+++ MultiplicationTableGame/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Player needs to select a multiplication table to practice
+    @State private var multiplicationTable: Int = 2
+    
+    // Player should be asked how many questions they want to be asked: 5, 10, or 20
+    let amountOfQuestions = [5, 10, 15, 20]
+    @State private var selectedQuestionAmount = 5
+    
+    // Randomly generate as many questions as they asked for, within the difficulty range they asked for
+    
+    // Animation State
+    @State private var symbolAnimationAmount = 1.0
+    @State private var animating = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Times Table") {
+                    Stepper(value: $multiplicationTable, in: 2...12, step: 1) {
+                        HStack {
+                            Text("")
+                            Image(systemName: "\(multiplicationTable).square")
+                                .font(.title)
+                                .scaleEffect(animating ? 1.3 : 1.0)
+                        }
+                    }
+                    .onChange(of: multiplicationTable) {
+                        withAnimation(.easeOut) {
+                            animating = true
+                        }
+                        
+                        Task { @MainActor in
+                            try? await Task.sleep(nanoseconds: 667_000_000)
+                            withAnimation(.easeIn) {
+                                animating = false
+                            }
+                        }
+                        
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.33) {
+//                            withAnimation(.easeOut) {
+//                                animating = false
+//                            }
+//                        }
+                    }
+                }
+                
+                Section("Number of Questions") {
+                    Picker("", selection: $selectedQuestionAmount) {
+                        ForEach(amountOfQuestions, id: \.self) { amount in
+                            Text("\(amount)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+            }
+            .navigationTitle("⨷ Multiplier Master")
         }
-        .padding()
     }
 }
 
