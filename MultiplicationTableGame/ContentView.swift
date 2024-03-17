46a32aa5022bb0c2b709bc64905222409313b748
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-03-17 03:17:51 -0700

    Added boxes to display the numbers being multiplied and a textfield for players to input an answer

--- MultiplicationTableGame/ContentView.swift
+++ MultiplicationTableGame/ContentView.swift
@@ -16,42 +16,23 @@ struct ContentView: View {
     @State private var selectedQuestionAmount = 5
     
     // Randomly generate as many questions as they asked for, within the difficulty range they asked for
-    
-    // Animation State
-    @State private var symbolAnimationAmount = 1.0
-    @State private var animating = false
+    @State private var answer: String = ""
     
     var body: some View {
         NavigationStack {
-            Form {
+            VStack {
+                Spacer()
                 Section("Times Table") {
                     Stepper(value: $multiplicationTable, in: 2...12, step: 1) {
                         HStack {
                             Text("")
                             Image(systemName: "\(multiplicationTable).square")
                                 .font(.title)
-                                .scaleEffect(animating ? 1.3 : 1.0)
                         }
-                    }
-                    .onChange(of: multiplicationTable) {
-                        withAnimation(.easeOut) {
-                            animating = true
-                        }
-                        
-                        Task { @MainActor in
-                            try? await Task.sleep(nanoseconds: 667_000_000)
-                            withAnimation(.easeIn) {
-                                animating = false
-                            }
-                        }
-                        
-//                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.33) {
-//                            withAnimation(.easeOut) {
-//                                animating = false
-//                            }
-//                        }
+                        .scaleUpDown(multiplicationTable: multiplicationTable)
                     }
                 }
+                .padding([.bottom, .horizontal])
                 
                 Section("Number of Questions") {
                     Picker("", selection: $selectedQuestionAmount) {
@@ -61,10 +42,86 @@ struct ContentView: View {
                     }
                     .pickerStyle(.segmented)
                 }
+                .padding([.bottom, .horizontal])
+                
+                Text("What is...?")
+                
+                HStack {
+                    Spacer()
+                    NumberBox(label: multiplicationTable)
+                    Spacer()
+                    Image(systemName: "xmark")
+                        .font(.largeTitle)
+                    Spacer()
+                    NumberBox(label: multiplicationTable)
+                    Spacer()
+                }
+                
+                Section("Type your answer here"){
+                    TextField("Type your answer here", text: $answer)
+                        .keyboardType(.numberPad)
+                        .textFieldStyle(.roundedBorder)
+
+                }
+                .padding([.top, .horizontal])
+                
+                Spacer()
+                Spacer()
                 
             }
             .navigationTitle("⨷ Multiplier Master")
         }
+    }
+}
+
+// -- VIEWS --
+
+struct NumberBox: View {
+    let label: Int
+    let cornerSize: CGSize = .init(width: 20, height: 20)
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerSize: cornerSize)
+                .stroke(Color.red, style: StrokeStyle(lineWidth: 5, lineCap: .round))
+                .frame(width: 100, height: 100)
+            Text("\(label)")
+                .font(.largeTitle)
+        }
+    }
+}
+
+// -- MODIFIERS --
+
+// Scales the times table number up and down
+@MainActor
+struct ScaleUpDown: ViewModifier {
+    @State private var animating = false
+    var multiplicaitonTable: Int
+    var delay: Double
+    
+    func body(content: Content) -> some View {
+        content
+            .scaleEffect(animating ? 1.3: 1.0)
+            .onChange(of: multiplicaitonTable) {
+                
+                withAnimation(.easeOut) {
+                    animating = true
+                }
+                Task {
+                    try? await Task.sleep(nanoseconds: UInt64(delay * 1_000_000_000))
+                    withAnimation(.easeIn) {
+                        animating = false
+                    }
+                }
+                
+            }
+    }
+}
+
+extension View {
+    func scaleUpDown(multiplicationTable: Int, delay: Double = 0.5) -> some View {
+        modifier(ScaleUpDown(multiplicaitonTable: multiplicationTable, delay: delay))
     }
 }
 
