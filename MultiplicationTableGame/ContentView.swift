f9845a30abc7fa8d667ced85e4a41cc699949a85
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-03-18 02:01:59 -0700

    Implemented game logic and added a way to see the player's score and turn

--- MultiplicationTableGame/ContentView.swift
+++ MultiplicationTableGame/ContentView.swift
@@ -13,11 +13,24 @@ struct ContentView: View {
     
     // Player should be asked how many questions they want to be asked: 5, 10, or 20
     let amountOfQuestions = [5, 10, 15, 20]
-    @State private var selectedQuestionAmount = 5
+    @State private var selectedQuestionAmount: Int = 5
+    
     
     // Randomly generate as many questions as they asked for, within the difficulty range they asked for
-    @State private var answer: String = ""
     @State private var questions: [Question] = []
+    @State private var currentQuestion = Question()
+    
+    // Record player's answer
+    @State private var answer: String = ""
+    
+    // Game data
+    @State private var score: Int = 0
+    @State private var turn: Int = 0
+    
+    // Alerts
+    @State private var gameIsOver = false
+    @State private var isCorrect = false
+    @State private var isIncorrect = false
     
     func generateQuestions(multplicationTable: Int, amountOfQuestions: Int) -> [Question] {
         var questions: [Question] = Array(repeating: Question(), count: amountOfQuestions)
@@ -28,6 +41,37 @@ struct ContentView: View {
         }
         
         return questions
+    }
+    
+    func startGame() {
+        resetGame()
+        questions = generateQuestions(multplicationTable: multiplicationTable, amountOfQuestions: selectedQuestionAmount)
+        currentQuestion = questions.first!
+    }
+    
+    func checkAnswer() {
+        let answer: Int = Int(answer)!
+        
+        if answer == currentQuestion.answer {
+            score += 1
+            isCorrect = true
+        } else {
+            isIncorrect = true
+        }
+        
+        turn += 1
+        if turn == selectedQuestionAmount { gameIsOver = true }
+        
+        currentQuestion = questions[turn-1]
+    }
+    
+    func resetGame() {
+        questions = []
+        currentQuestion = Question()
+        answer = ""
+        score = 0
+        turn = 0
+        gameIsOver = false
     }
     
     var body: some View {
@@ -58,39 +102,50 @@ struct ContentView: View {
                 
                 Text("What is...?")
                 
-                HStack {
-                    Spacer()
-                    NumberBox(label: multiplicationTable)
-                    Spacer()
-                    Image(systemName: "xmark")
-                        .font(.largeTitle)
-                    Spacer()
-                    NumberBox(label: multiplicationTable)
-                    Spacer()
-                }
-                
+                QuestionDisplay(question: currentQuestion)
+                    
                 Section("Type your answer here"){
                     TextField("Type your answer here", text: $answer)
                         .keyboardType(.numberPad)
                         .textFieldStyle(.roundedBorder)
-
+                    
                 }
                 .padding([.top, .horizontal])
                 
                 Button {
-                    questions = generateQuestions(multplicationTable: multiplicationTable, amountOfQuestions: selectedQuestionAmount)
-                    for index in 0..<questions.count {
-                        print("\(questions[index]) || answer: \(questions[index].answer)")
-                    }
+                    startGame()
+                    //                    for index in 0..<questions.count {
+                    //                        print("\(questions[index]) || answer: \(questions[index].answer)")
+                    //                    }
                 } label: {
-                    Text("Generate")
+                    Text("Start Game")
                 }
+                .padding()
+                
+                Text("Turn: \(turn)/\(selectedQuestionAmount)")
+                Text("Score: \(score)")
                 
                 Spacer()
                 Spacer()
                 
             }
             .navigationTitle("⨷ Multiplier Master")
+        }
+        .onSubmit(checkAnswer)
+        .alert("That's correct! 🥳", isPresented: $isCorrect) {
+            Button("Continue") { answer = "" }
+        } message: {
+            Text("Your score is \(score)")
+        }
+        .alert("That's incorrect! 🥺", isPresented: $isIncorrect) {
+            Button("Continue") { answer = "" }
+        } message: {
+            Text("Your score is \(score)")
+        }
+        .alert("Game Over!", isPresented: $gameIsOver) {
+            Button("Reset", action: resetGame)
+        } message: {
+            Text("Your final score is \(score)/\(selectedQuestionAmount)")
         }
     }
 }
@@ -105,24 +160,49 @@ struct Question {
 
 // -- VIEWS --
 
+struct QuestionDisplay: View {
+    let question: Question
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            NumberBox(label: question.firstNumber)
+            
+            Spacer()
+            
+            Image(systemName: "xmark")
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            NumberBox(label: question.secondNumber)
+            
+            Spacer()
+        }
+    }
+}
+
 struct NumberBox: View {
     let label: Int
-    let cornerSize: CGSize = .init(width: 20, height: 20)
+    var color: Color = .blue
+    var cornerSize: CGSize = .init(width: 20, height: 20)
     
     var body: some View {
         ZStack {
             RoundedRectangle(cornerSize: cornerSize)
-                .stroke(Color.red, style: StrokeStyle(lineWidth: 5, lineCap: .round))
+                .stroke(color, style: StrokeStyle(lineWidth: 5, lineCap: .round))
                 .frame(width: 100, height: 100)
             Text("\(label)")
                 .font(.largeTitle)
+            
         }
     }
 }
 
 // -- MODIFIERS --
 
-// Scales the times table number up and down
+// Scales the picker image up and down
 @MainActor
 struct ScaleUpDown: ViewModifier {
     @State private var animating = false
