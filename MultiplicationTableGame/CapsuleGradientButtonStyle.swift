b7693cf41970566026ea3ec14d57588848afbe31
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-03-20 18:02:49 -0700

    A custom button with a sun like design

--- MultiplicationTableGame/CapsuleGradientButtonStyle.swift
+++ MultiplicationTableGame/CapsuleGradientButtonStyle.swift
@@ -7,12 +7,42 @@
 
 import SwiftUI
 
-struct CapsuleGradientButtonStyle: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CapsuleGradientButtonStyle: ButtonStyle {
+    @State private var isPressed = false
+    var startRadius: CGFloat = 0
+    var endRadius: CGFloat = 50
+    
+    func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .padding()
+                .background(
+                    Capsule()
+                        .fill(RadialGradient(gradient: Gradient(colors: [.red, .yellow, .blue]), center: .center, startRadius: isPressed ? 2 : startRadius, endRadius: isPressed ? 150 : endRadius)) // Adjust radii based on isPressed
+                )
+                .foregroundStyle(.white)
+                .font(.headline)
+                .clipShape(Capsule())
+                .scaleEffect(isPressed ? 1.1 : 1)
+                .animation(.easeOut, value: isPressed)
+                .simultaneousGesture(
+                    TapGesture()
+                        .onEnded { _ in
+                            withAnimation { isPressed.toggle() }
+                        }
+                )
+        }
+}
+
+extension View {
+    func sunButton() -> some View {
+        buttonStyle(CapsuleGradientButtonStyle())
     }
 }
 
 #Preview {
-    CapsuleGradientButtonStyle()
+    Button("Start Game") {
+        // Placeholder action
+        print("Button tapped!")
+    }
+    .buttonStyle(CapsuleGradientButtonStyle())
 }
